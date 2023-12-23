d58529a10c6b38ff9c4d88f949453adbcb2266b4
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2023-12-23 21:47:23 +0200

    Solved problems from lab 02

---
+++ 02.1-ConditionalStatements-Lab/07-AreaOfFigures/Program.cs
@@ -0,0 +1,27 @@
+﻿string shape = Console.ReadLine();
+double area = 0.0;
+
+if (shape == "square")
+{
+    double side = double.Parse(Console.ReadLine());
+    area = side * side;
+}
+else if (shape == "rectangle")
+{
+    double length =  double.Parse(Console.ReadLine());
+    double width =  double.Parse(Console.ReadLine());
+    area = length * width;
+}
+else if (shape == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+    area = radius * radius * Math.PI;
+}
+else
+{
+    double side = double.Parse(Console.ReadLine());
+    double height = double.Parse(Console.ReadLine());
+    area = (side * height) / 2;
+}
+
+Console.WriteLine($"{area:f3}");
